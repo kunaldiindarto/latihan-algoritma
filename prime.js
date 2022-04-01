c1976c46dcfc07c7226f3f30d866baef23be7188
kunaldiindarto <kunaldi.ind@gmail.com>
2022-04-01 14:30:38 +0700

    edit prime

--- prime.js
+++ prime.js
@@ -6,10 +6,9 @@ const rl = readline.createInterface({ input, output });
 const inputNumber = () => {
   return new Promise((resolve, reject) => {
     rl.question("Masukan input angka \n", (answer) => {
-      if (isNaN(answer)) {
-        rl.close();
-        reject(new Error("Input yang anda masukan bukan angka"));
-      } else resolve(Math.sign(parseInt(answer)));
+      if (!isNaN(answer)) return resolve(answer);
+      rl.close();
+      reject(new Error("Input yang anda masukan bukan angka"));
     });
   });
 };
@@ -17,11 +16,8 @@ const inputNumber = () => {
 const resume = () => {
   return new Promise((resolve, reject) => {
     rl.question("Apakah lagi? (Tekan Y, selain itu close) \n", (answer) => {
-      if (answer.toString().toLowerCase() === "y") {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
+      if (answer.toString().toLowerCase() === "y") return resolve(true);
+      resolve(false);
     });
   });
 };
@@ -41,33 +37,33 @@ const repeat = () => {
 const Main = async () => {
   try {
     const x = await processNumber();
-    if (x === 1 || x <= 3) {
+
+    if (x === 1 && x <= 3) {
+      console.log("1");
       console.log(true);
-
       const again = await repeat();
-      if (again === true) Main();
-      else rl.close();
-    } else if (x < 1) {
+      if (again === true) return Main();
+      return rl.close();
+    }
+    if (x < 1) {
+      console.log("2");
       console.log(false);
-
       const again = await repeat();
-      if (again === true) Main();
-      else rl.close();
-    } else {
+      if (again === true) return Main();
+      return rl.close();
+    }
+    if (x > 3) {
       let pembagi = 0;
       for (let i = 1; i <= x; i++) {
-        if (x % i === 0) {
-          // jadi ini operator pembagi apakah ada sisa atau tidak, jika ada sisa !=0 jika tidak ada sisa ==0
-          pembagi++;
-        }
+        // jadi ini operator pembagi apakah ada sisa atau tidak, jika ada sisa !=0 jika tidak ada sisa ==0
+        if (x % i === 0) pembagi++;
       }
       if (pembagi == 2) console.log(true);
       else console.log(false);
 
       const again = await repeat();
-
-      if (again === true) Main();
-      else rl.close();
+      if (again === true) return Main();
+      return rl.close();
     }
   } catch (err) {
     console.log(err);
